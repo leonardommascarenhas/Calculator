09c16eb8b4dfe1d3e2484009d5fa7c270ef7aaef
leonardo <leonardommascarenhas@gmail.com>
2022-07-01 16:36:01 -0300

    alterações no css e js

--- script.js
+++ script.js
@@ -1,30 +1,42 @@
 let num1 = "";
 let num2 = "";
 let operator = "";
+
 const input = document.querySelector(".operador");
 const buttons = document.querySelectorAll("button");
+
 buttons.forEach((button) => {
   button.addEventListener("click", populateInput);
 });
 
 function populateInput(e) {
+  if (e.target.classList.contains("clear")) {
+    clear();
+  }
   if (e.target.classList.contains("symbol")) {
-    input.innerHTML = "";
+    clear();
     operator = e.target.innerHTML;
   }
   if (operator === "") {
     if (e.target.classList.contains("number")) {
       num1 = input.innerHTML += e.target.innerHTML;
-      console.log(`num1: ${num1}`);
     }
   } else {
     if (e.target.classList.contains("number")) {
       num2 = input.innerHTML += e.target.innerHTML;
-      console.log(`num2: ${num2}`);
     }
   }
   if (e.target.classList.contains("equals")) {
+    equals();
+  }
+}
+
+function equals() {
+  if (num1 != "" && operator != "" && num2 != "") {
     input.innerHTML = calculate(operator, num1, num2);
+    num1 = input.innerHTML;
+  } else {
+    return (input.innerHTML = "ERROR");
   }
 }
 
